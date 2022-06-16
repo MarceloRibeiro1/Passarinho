81a1cc3446ba37413d4f65b8f34176ac01eb0d13
MarceloRibeiro1 <86325217+MarceloRibeiro1@users.noreply.github.com>
2022-06-16 10:03:12 -0300

    process env

--- src/lib/socketio.ts
+++ src/lib/socketio.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = import.meta.env.VITE_API_URL;
+const URL = process.env.VITE_API_URL as string;
 const socket = io(URL, { autoConnect: true });
 
 export default socket;
