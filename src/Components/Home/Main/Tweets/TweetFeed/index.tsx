20e7f4f541799873c51e8662c07f8cbe14d5ec26
MarceloRibeiro1 <86325217+MarceloRibeiro1@users.noreply.github.com>
2022-06-16 13:05:32 -0300

    map

--- src/Components/Home/Main/Tweets/TweetFeed/index.tsx
+++ src/Components/Home/Main/Tweets/TweetFeed/index.tsx
@@ -70,11 +70,11 @@ export function TweetFeed() {
       }
       <div className="flex-grow w-full">
       {
-        /*
+        
         tweetList.map((tweet) => 
           <Tweet key={tweet.id} className="hover:bg-slate-100" postInfo={tweet} />
         )
-        */
+        
       }
       <button onClick={() => console.log(tweetList)} className="teste p-2 bg-blue-600"> oi botão aqui</button>
       </div>
