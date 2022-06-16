293feb8db478de4048586594314f98f687495947
MarceloRibeiro1 <86325217+MarceloRibeiro1@users.noreply.github.com>
2022-06-16 10:42:01 -0300

    import meta

--- src/Pages/LoginPage/index.tsx
+++ src/Pages/LoginPage/index.tsx
@@ -27,10 +27,7 @@ function LoginPage() {
         <div className="flex flex-col items-center w-fit py-4">
           <LoginButton
             loginText="Inscreva-se pelo email"
-            loginDirectFunction={() => {
-              handleLoginMedium("CREATE_NEW_ACCOUNT")
-              console.log(import.meta.env.VITE_API_URL)
-            }}
+            loginDirectFunction={() => handleLoginMedium("CREATE_NEW_ACCOUNT")}
           />
 
           <span className="py-4">Já tem uma conta?</span>
--- src/lib/socketio.ts
+++ src/lib/socketio.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = process.env.VITE_API_URL as string;
+const URL = import.meta.env.VITE_API_URL;
 const socket = io(URL, { autoConnect: true });
 
 export default socket;
